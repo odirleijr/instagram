1d8bbf5fd859b73b2713524857af5eecf36e6703
odirlei J S Júnior <odirlei.sousaa@gmail.com>
2020-03-13 15:17:08 -0300

    [Feature] Translation setting i18n

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Instagram
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = 'Brasilia'
+    config.i18.default_locale = 'pt-BR'
   end
 end
